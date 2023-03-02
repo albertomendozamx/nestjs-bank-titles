7830089350a3d8bbbc27e7857e581f70d5b60a9f
Alberto Mendoza <mendozaene@gmail.com>
2023-03-02 17:01:15 -0600

    :construction: valida tipos

---
+++ src/dtos/pago.enum.ts
@@ -0,0 +1,4 @@
+export enum Pago {
+  Y = 'Y',
+  N = 'N',
+}
--- src/dtos/title.dto.ts
+++ src/dtos/title.dto.ts
@@ -1,22 +1,63 @@
+import {
+  IsNumber,
+  IsString,
+  IsNotEmpty,
+  Min,
+  Length,
+  IsDateString,
+  IsEnum,
+} from 'class-validator';
+import { OmitType, PartialType } from '@nestjs/mapped-types';
+
 import { Description } from './description.enum';
 import { IDTitle } from './title.enum';
+import { Pago } from './pago.enum';
 
-export interface Title {
-  id: number;
-  idtitulo: IDTitle;
-  titulo: Description;
-  clasificacion: string;
-  valor: number;
-  fecha_creacion: string;
-  fecha_vencimiento: string;
-  pagocuota: string;
+export class Title {
+  @IsNumber()
+  readonly id: number;
+
+  @IsNotEmpty()
+  @IsEnum(IDTitle)
+  readonly idtitulo: IDTitle;
+
+  @IsNotEmpty()
+  @IsEnum(Description)
+  readonly titulo: Description;
+
+  @IsNotEmpty()
+  @IsString()
+  readonly clasificacion: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  @Min(1)
+  readonly valor: number;
+
+  @IsNotEmpty()
+  @IsDateString()
+  readonly fecha_creacion: string;
+
+  @IsNotEmpty()
+  @IsDateString()
+  readonly fecha_vencimiento: string;
+
+  @IsNotEmpty()
+  @IsEnum(Pago)
+  @Length(1)
+  readonly pagocuota: Pago;
 }
 
-export type TitleDto = Omit<Title, 'id'>;
+export class TitleDto extends OmitType(Title, ['id']) {}
 
-export type TitleUpdateDto = Partial<TitleDto>;
+export class TitleUpdateDto extends PartialType(TitleDto) {}
 
-export type MoveDto = {
+export class MoveDto {
+  @IsNotEmpty()
+  @IsDateString()
   from: string;
+
+  @IsNotEmpty()
+  @IsDateString()
   to: string;
-};
+}
